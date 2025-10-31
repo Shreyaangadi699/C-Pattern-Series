09884a027fe4032e92c42b3ab83cf0a7973afbef
Shreya Angadi <shreyaangadi699@gmail.com>
2025-10-31 09:53:44 -0400

    Added Pattern 1 - Square using stars
    This program prints a square pattern using nested for loops in C.

---
+++ Pattern1_square.c
@@ -0,0 +1,20 @@
+//Program: print a square using stars
+//Author: Shreya Angadi
+
+#include<stdio.h>
+int main()
+{
+  int i,j,n;
+  printf("Enter number of rows : ");
+  scanf("%d",&n);
+  
+  for(i=1;i<=n;i++)
+  {
+    for(j=1;j<=n;j++)
+    {
+      printf("*");
+    }
+      printf("\n");
+    }
+    return 0;
+}
